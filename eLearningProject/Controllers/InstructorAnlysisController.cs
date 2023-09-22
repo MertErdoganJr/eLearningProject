797e5ab1d185c91af8c2ab43bbc9ac825223e4db
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-09-22 21:39:18 +0300

    SQL to Linq

--- eLearningProject/Controllers/InstructorAnlysisController.cs
+++ eLearningProject/Controllers/InstructorAnlysisController.cs
@@ -10,7 +10,7 @@ namespace eLearningProject.Controllers
     public class InstructorAnlysisController : Controller
     {
         eLearningContext context = new eLearningContext();
-        
+
         public ActionResult Index()
         {
             return View();
@@ -19,13 +19,18 @@ namespace eLearningProject.Controllers
         public PartialViewResult InstructorPanelPartial()
         {
             int id = 1;
-            var values = context.Instructors.Where(x=>x.InstructorID == id).ToList();
+            var values = context.Instructors.Where(x => x.InstructorID == id).ToList();
             return PartialView(values);
         }
 
         public PartialViewResult CommetPartial()
         {
-            return PartialView();
+
+            var v1 = context.Instructors.Where(x => x.Name == "Ahmet" && x.Surname == "Ölçen").Select(y => y.InstructorID).FirstOrDefault();
+            var v2 = context.Courses.Where(x => x.InstructorID == v1).Select(y => y.CourseID).ToList();
+            var v3 = context.Comments.Where(x=>v2.Contains(x.CourseID)).ToList();
+
+            return PartialView(v3);
         }
     }
 }
