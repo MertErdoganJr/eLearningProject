5e545c480a40e64688e6067093989ef1f7101e3d
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-10-02 23:57:50 +0300

    Student Page Design

---
+++ eLearningProject/Controllers/StudentPanelLayoutController.cs
@@ -0,0 +1,54 @@
+﻿using eLearningProject.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eLearningProject.Controllers
+{
+    public class StudentPanelLayoutController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialPageRowTitle()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialPreloader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+    }
+}
